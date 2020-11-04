d7d909daec428bc1bbaf3402eb59bd2904964fa6
jsheo <jsheo@konai.com>
2020-11-04 16:57:07 +0900

    초기 코드 커밋

---
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/EatGoProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.eatgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatGoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatGoProjectApplication.class, args);
+	}
+
+}
