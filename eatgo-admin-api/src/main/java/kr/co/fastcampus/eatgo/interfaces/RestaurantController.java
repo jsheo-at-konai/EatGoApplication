1225c041ced2cec23bd4940374ad996bf5409ed3
jsheo <jsheo@konai.com>
2020-11-05 21:30:58 +0900

    프론트엔드 실습

--- eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -10,6 +10,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class RestaurantController {
 
@@ -35,5 +36,13 @@ public class RestaurantController {
 
         URI location = new URI("/restaurants/" + restaurant.getId());
         return ResponseEntity.created(location).body("{}");
+    }
+
+    @PatchMapping("/restaurants/{id}")
+    public String update(@PathVariable("id") Long id, @RequestBody Restaurant resource) {
+        String name = resource.getName();
+        String address = resource.getAddress();
+        restaurantService.updateRestaurant(id, name, address);
+        return "{}";
     }
 }
